8c44efb36ba16e642676d3618fb8ee276c0794d0
Ayush Bhankale <Aayush.Bhankale@SRKayCG.com>
2020-10-11 14:18:04 +0530

    refactor: category

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryDetailsComponent } from './category-details/category-details.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'category', pathMatch: 'prefix'},
+  { path:'category', component:CategoryDetailsComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- webapp/src/app/category-details/category-details.component.ts
+++ webapp/src/app/category-details/category-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryServiceService } from '../service/category-service.service'
 
 @Component({
   selector: 'app-category-details',
@@ -7,9 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryDetailsComponent implements OnInit {
 
-  constructor() { }
+  public insertCatagory: any;
+  public catagoryList: any;
+  public updateCategory: any;
+  public updateFlag: any = false;
+  public createFlag: any = true;
+  public toUpdateList: any;
+  constructor(
+    public _CategoryServiceService: CategoryServiceService
+  ) { }
 
   ngOnInit(): void {
+    this.getCatagory();
   }
 
+  createCatagory() {
+    console.log('insertCatagory', this.insertCatagory);
+    const body = {};
+    body['name'] = this.insertCatagory;
+    this._CategoryServiceService.insertCategory(body).subscribe((response) => {
+      this.getCatagory();
+      this.insertCatagory = '';      
+    })
+  }
+
+  getCatagory() {
+    this._CategoryServiceService.getCategory().subscribe((response) => {
+      // console.log('response---',response);
+      this.catagoryList = response;
+    })
+  }
+
+  editCatagory(list) {
+    this.updateFlag = true;
+    this.createFlag = false;
+    this.toUpdateList = list;
+    this.updateCategory = list.name;
+    // console.log('LIST', list);
+    
+  }
+  updateCatagory() {
+    const body = {};
+    body['name'] = this.updateCategory;
+    const id = this.toUpdateList.id;
+  this._CategoryServiceService.updateCategory(body, id).subscribe(response => {
+    // console.log('Updated', response);
+    this.goTocreateCatagory();
+    this.getCatagory();
+    
+  })
+  }
+
+  deleteCatagory(list) {
+    const  id = list.id;
+    this._CategoryServiceService.deleteCategory(id).subscribe(response => {
+      this.getCatagory();
+    })
+  }
+
+  goTocreateCatagory() {
+    this.updateFlag = false;
+    this.createFlag = true;
+  }
 }
