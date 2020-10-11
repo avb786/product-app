8bc53275074d19b9117d31e93bc26d8bcfa50d21
Ayush Bhankale <Aayush.Bhankale@SRKayCG.com>
2020-10-11 12:00:20 +0530

    refator: service added

--- webapp/src/app/service/category-service.service.ts
+++ webapp/src/app/service/category-service.service.ts
@@ -14,6 +14,11 @@ export class CategoryServiceService {
     return this._http.post(url, body);
   }
 
+  updateCategory(body, id) { 
+    const url = `http://localhost:8081/api/category/update?id=${id}`;
+    return this._http.post(url, body);
+  }
+
   getCategory() { 
     const url = 'http://localhost:8081/api/category/get';
     return this._http.get(url);
@@ -28,4 +33,25 @@ export class CategoryServiceService {
     const url = 'http://localhost:8081/api/product/get';
     return this._http.get(url);
   }
+
+  updateProduct(body, id) { 
+    const url = `http://localhost:8081/api/category/update?id=${id}`;
+    return this._http.post(url, body);
+  }
+
+  getCategoryForProduct() { 
+    const url = 'http://localhost:8081/api/category/get/menu';
+    return this._http.get(url);
+  }
+
+  deleteCategory(id) {
+    const url = `http://localhost:8081/api/category/delete?id=${id}`;
+    return this._http.delete(url);
+  }
+
+  deleteProduct(id) {
+    const url = `http://localhost:8081/api/product/delete?id=${id}`;
+    return this._http.delete(url);
+  }
+
 }
